36c99dccc675de4e143ecf42e7931e40905b1c34
Oskar <osk.kierys@gmail.com>
2025-04-23 13:31:58 +0200

    finished team.hpp

--- src/models/team.hpp
+++ src/models/team.hpp
@@ -8,9 +8,50 @@
 class Team
 {
 public:
+    Team(int id, 
+        const std::string& name, 
+        const std::string& nationality, 
+        int foundedYear, 
+        int championships, 
+        double yearlyBudget,
+        double driverBudget);
 
+    Team(const std::string& name, 
+        const std::string& nationality,
+        int foundedYear,
+        int championships = 0,
+        double yearlyBudget = 0.0,
+        double driverBudget = 0.0);
+
+    // Getters
+    int getId() const { return m_id; }
+    const std::string& getName() const { return m_name; }
+    const std::string& getNationality() const { return m_nationality; }
+    int getFoundedYear() const { return m_foundedYear; }
+    int getChampionships() const { return m_championships; }
+    double getYearlyBudget() const { return m_yearlyBudget; }
+    double getDriverBudget() const { return m_driverBudget; }
+
+    // Setters
+    void setName(const std::string& name) { m_name = name; }
+    void setNationality(const std::string& nationality) { m_nationality = nationality; }
+    void setFoundedYear(int year) { m_foundedYear = year; }
+    void setChampionships(int championships) { m_championships = championships; }
+    void setYearlyBudget(double budget) { m_yearlyBudget = budget; }
+    void setDriverBudget(double budget) { m_driverBudget = budget; }
+
+    // Other methods
+    int getAge() const;
+    double getDriverBudgetPercwentage() const;
 
 private:
+    int m_id = -1;
+    std::string m_name;
+    std::string m_nationality;  
+    int m_foundedYear;
+    int m_championships;
+    double m_yearlyBudget;
+    double m_driverBudget;
 };
 
 using TeamPtr = std::shared_ptr<Team>;
